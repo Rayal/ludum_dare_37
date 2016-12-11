39889a92e49e29ee0925bc2d43ead195f545379a
ladmin <ladmin>
2016-12-11 23:03:08 +0200

    Camera following

---
+++ Assets/code/follow_player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow_player : MonoBehaviour {
+	public GameObject pc_object;
+	public float world_length;
+	public bool camera_unlimited = true;
+
+	private float camera_limit;
+	// Use this for initialization
+	void Start () {
+		float len = Camera.main.orthographicSize * Camera.main.aspect * 2;
+		Debug.Log (len);
+		camera_limit = (world_length - len) / 2;
+		follow ();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		follow ();
+	}
+
+	void follow () {
+		float offset = pc_object.transform.position.x - transform.position.x;
+		if ((offset != 0 && pc_object.transform.position.x > -camera_limit && pc_object.transform.position.x < camera_limit) ||
+			(offset != 0 && camera_unlimited)){
+			transform.Translate (offset, 0, 0);
+		}
+	}
+}
--- Assets/code/pc_movement_normal.cs
+++ Assets/code/pc_movement_normal.cs
@@ -2,25 +2,29 @@
 using System.Collections;
 
 public class pc_movement_normal : MonoBehaviour {
-	public Vector3 startposition;
-	Vector3 mouse_pos = new Vector3(0, 0, 0);
+	public Vector3 start_position;
+	public bool use_def_pos = true;
+	Vector3 mouse_pos;
 	// Use this for initialization
 	void Start () {
-		transform.position = startposition;
+		if (!use_def_pos)
+			transform.position = start_position;
+		mouse_pos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonUp (1)) {
+		if (Input.GetMouseButton (1)) {
 			mouse_pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			mouse_pos.y = transform.position.y;
 			mouse_pos.z = transform.position.z;
 		}
 		float delta = mouse_pos.x - transform.position.x;
 		if (delta != 0) {
+			gameObject.GetComponent<SpriteRenderer> ().flipX = (delta < 0);
 			transform.Translate (Time.deltaTime * delta, 0, 0);
 		}
 
-		//TODO: Action for mouse button 1
+		//TODO: Action for mouse button 0
 	}
 }
