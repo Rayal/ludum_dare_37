725c28f9d4a91fd2679f52fd2a0bc3e833f2d834
Jürmo Mehine <jyrmo.mehine@gmail.com>
2016-12-12 01:07:04 +0200

    Additions to dialog system.

--- SafeSpace/Assets/DialogController.cs
+++ SafeSpace/Assets/DialogController.cs
@@ -5,17 +5,38 @@ using UnityEngine.UI;
 
 public class DialogController : MonoBehaviour {
 	public Text text;
+	public GameObject game;
+	public Button[] choices;
 
 	void Start () {
+		DisableChoices ();
 	}
 	
 	void Update () {
 	}
 
-	public void RenderNode(string id) {
+	public void RenderNode (string id) {
 		// TODO: move text with camera?
+
+		DisableChoices ();
+
 		DialogNode node = DialogDatabase.GetNode(id);
 
+		game.SendMessage ("ApplyConsequence", node.consequence);
 		text.text = node.text;
+		int idxEdge = 0;
+		foreach (DialogEdge edge in node.edges) {
+			float y = choices [idxEdge].transform.position.y;
+			choices [idxEdge].transform.position = new Vector2 (231.5f, y);
+			choices [idxEdge].GetComponentInChildren<Text> ().text = edge.description;
+			idxEdge++;
+		}
+	}
+
+	private void DisableChoices () {
+		foreach (Button choice in choices) {
+			float y = choice.transform.position.y;
+			choice.transform.position = new Vector2 (10000, y);
+		}
 	}
 }
