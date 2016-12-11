d1f7bac090d32c26078aec63dddb5b912930fb29
ladmin <ladmin>
2016-12-11 16:27:44 +0200

    New assets

---
+++ Assets/code/change_Screen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class change_Screen : MonoBehaviour {
+	Vector3 camera_pos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+	}
+}
---
+++ Assets/code/observable_object.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class observable_object : MonoBehaviour {
+	public Vector3 startposition;
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = startposition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseUp()
+	{
+		Debug.Log ("Mouse clicked me.");
+		var instance = Instantiate (target); // Resources.Load ("SPR_ME")
+		//instance = instance;
+	}
+
+}
