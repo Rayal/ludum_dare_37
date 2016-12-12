125ab37af68771fd9196b91e519e6dd17c32ee7c
Jürmo Mehine <jyrmo.mehine@gmail.com>
2016-12-12 10:43:48 +0200

    Added dialog code to main project assets.

---
+++ Assets/code/DialogEdge.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogEdge {
+	public string description;
+	public string destinationId;
+
+	public DialogEdge (string description, string destinationId) {
+		this.description = description;
+		this.destinationId = destinationId;
+	}
+}
